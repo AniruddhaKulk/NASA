7d67f371590ce11310953e4651733917ff9d0192
AniruddhaKulk <anikulki@gmail.com>
2020-11-15 12:20:06 +0530

    Create model class to hold the data related to NASA

---
+++ app/src/main/java/com/anikulki/nasa/model/NasaModel.kt
@@ -0,0 +1,14 @@
+package com.anikulki.nasa.model
+
+class NasaModel : ArrayList<NASAImagesItem>()
+
+data class NASAImagesItem(
+    val copyright: String,
+    val date: String,
+    val explanation: String,
+    val hdurl: String,
+    val media_type: String,
+    val service_version: String,
+    val title: String,
+    val url: String
+)
