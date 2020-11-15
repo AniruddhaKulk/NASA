aa3a6919a1a86b5ab157ec99526b233a5beb7ebb
AniruddhaKulk <anikulki@gmail.com>
2020-11-15 13:32:12 +0530

    On application launch open the GridFragment to show the NASA images in grid view.

--- app/src/main/java/com/anikulki/nasa/ui/MainActivity.kt
+++ app/src/main/java/com/anikulki/nasa/ui/MainActivity.kt
@@ -3,10 +3,31 @@ package com.anikulki.nasa.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anikulki.nasa.R
+import com.anikulki.nasa.ui.grid.GridFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openNasaGridFragment()
     }
+
+
+    private fun openNasaGridFragment(){
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        var fragment = supportFragmentManager.findFragmentByTag(GridFragment.TAG) as GridFragment?
+
+        if(fragment == null){
+            fragment = GridFragment.newInstance()
+            fragmentTransaction.replace(R.id.containerView, fragment, GridFragment.TAG)
+        }else{
+            fragmentTransaction.show(fragment)
+        }
+
+        fragmentTransaction.commit()
+    }
+
 }
--- app/src/main/java/com/anikulki/nasa/ui/grid/GridFragment.kt
+++ app/src/main/java/com/anikulki/nasa/ui/grid/GridFragment.kt
@@ -1,7 +1,6 @@
 package com.anikulki.nasa.ui.grid
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -68,7 +67,7 @@ class GridFragment: Fragment(R.layout.fragment_grid), NasaImageAdapter.OnItemCli
 
                 is State.Error -> {
                     binding.progressBar.visibility = View.GONE
-                    binding.recyclerView.visibility = View.VISIBLE
+                    binding.recyclerView.visibility = View.GONE
                     Toast.makeText(requireContext(), it.errorMessage, Toast.LENGTH_LONG).show()
                 }
             }
